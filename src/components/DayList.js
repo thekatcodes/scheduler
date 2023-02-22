aed4a54e353fb34ff79ec42531ff83833478fae7
Katie Liu <katieliu59@gmail.com>
2023-02-22 00:21:11 -0500

    Complete DayList function using map over days array to return DayListItem components

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,5 +1,21 @@
-import DayListItem from "./DayListItem"
+import React from "react";
+import DayListItem from "./DayListItem";
 
-export default function DayList() {
-    return <ul></ul>
+export default function DayList(props) {
+	let days = props.days;
+	let daysDisplay = days.map((day) => {
+		return (
+			<ul>
+				<DayListItem
+					key={day.id}
+					name={day.name}
+					spots={day.spots}
+					selected={day.name === props.day}
+					setDay={props.setDay}
+				/>
+			</ul>
+		);
+	});
+
+	return <ul>{daysDisplay}</ul>;
 }
