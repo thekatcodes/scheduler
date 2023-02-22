f10991d6b52b4b85484b215299a976646ae6afa5
Katie Liu <katieliu59@gmail.com>
2023-02-22 16:19:54 -0500

    Create InterviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,10 @@
+import "components/InterviewerList.scss";
+
+export default function InterviewerList() {
+	return (
+		<section className="interviewers">
+			<h4 className="interviewers__header text--light">Interviewer</h4>
+			<ul className="interviewers__list"></ul>
+		</section>
+	);
+}
