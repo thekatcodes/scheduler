f5d09932e7290bbd22241a57592308624e7b0a13
Katie Liu <katieliu59@gmail.com>
2023-02-21 23:52:44 -0500

    Create DayListItem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+	const dayClass = classNames(
+		"day-list__item",
+		{ "day-list__item--selected": props.selected },
+		{ "day-list__item--full": props.spots === 0 }
+    );
+    
+    const formatSpots = function() {
+        
+    }
+
+	return (
+		<li
+			className={dayClass}
+			onClick={() => props.setDay(props.name)}
+		>
+			<h2 className="text--regular">{props.name}</h2>
+			<h3 className="text--light">{props.spots} spots remaining</h3>
+		</li>
+	);
+}
