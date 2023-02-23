4fe24fd7e6438b5c424208c0745936e7cc8c151f
Katie Liu <katieliu59@gmail.com>
2023-02-23 14:49:22 -0500

    Complete Appointment component to display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,23 @@
 import React from "react";
-import 'components/Appointment/styles.scss'
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
-	return <article className="appointment"></article>;
+
+    const { time, interview } = props;
+	return (
+		<article className="appointment">
+			<Header time={time} />
+			{interview ? (
+				<Show
+					student={interview.student}
+					interviewer={interview.interviewer}
+				/>
+			) : (
+				<Empty />
+			)}
+		</article>
+	);
 }
