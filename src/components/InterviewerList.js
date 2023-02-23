2ab3d5b87d956fd4bc0649c4f49edc2e02b8ac1d
Katie Liu <katieliu59@gmail.com>
2023-02-22 19:57:24 -0500

    Add logic for InterviewerList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,26 @@
+import React from "react";
 import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
 
-export default function InterviewerList() {
+export default function InterviewerList(props) {
+ 
+	let interviewers = props.interviewers;
+	const interviewerDisplay = interviewers.map((interviewer) => {
+        return (
+            <InterviewerListItem
+                key={interviewer.id}
+                name={interviewer.name}
+                avatar={interviewer.avatar}
+                selected={interviewer.id===props.interviewer}	
+                setInterviewer={() => props.setInterviewer(interviewer.id)}
+            />
+			
+		);
+	});
 	return (
 		<section className="interviewers">
-			<h4 className="interviewers__header text--light">Interviewer</h4>
-			<ul className="interviewers__list"></ul>
+			<h4 className="interviewers__header text--light">Interviewers</h4>
+			<ul className="interviewers__list"> {interviewerDisplay}</ul>
 		</section>
 	);
 }
