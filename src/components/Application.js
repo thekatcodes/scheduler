4631aeb30ec71288f625262c70df41e8192f8a26
Katie Liu <katieliu59@gmail.com>
2023-02-22 22:09:43 -0500

    Refactor code to use value and onChange as prop names

--- src/components/Application.js
+++ src/components/Application.js
@@ -36,9 +36,9 @@ export default function Application(props) {
 				<hr className="sidebar__separator sidebar--centered" />
 				<nav className="sidebar__menu">
 					<DayList
-						days={days}
-						day={day}
-						setDay={setDay}
+                        days={days} 
+                        value={day} 
+                        onChange={setDay} 
 					/>
 				</nav>
 				<img
