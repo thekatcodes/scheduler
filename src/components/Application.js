e2b5dbbfc92e232c0a40ffe42ed27314b09314b3
Katie Liu <katieliu59@gmail.com>
2023-02-23 15:04:38 -0500

    Add mock data for appointments and use  them to render a schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,28 +2,77 @@ import React from "react";
 import { useState } from "react";
 
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 import "components/Application.scss";
 
-export default function Application(props) {
-    const [day, setDay] = useState('Monday');
+const days = [
+	{
+		id: 1,
+		name: "Monday",
+		spots: 2,
+	},
+	{
+		id: 2,
+		name: "Tuesday",
+		spots: 5,
+	},
+	{
+		id: 3,
+		name: "Wednesday",
+		spots: 0,
+	},
+];
 
-	const days = [
-		{
-			id: 1,
-			name: "Monday",
-			spots: 2,
+const appointments = {
+	1: {
+		id: 1,
+		time: "12pm",
+	},
+	2: {
+		id: 2,
+		time: "1pm",
+		interview: {
+			student: "Lydia Miller-Jones",
+			interviewer: {
+				id: 3,
+				name: "Sylvia Palmer",
+				avatar: "https://i.imgur.com/LpaY82x.png",
+			},
 		},
-		{
-			id: 2,
-			name: "Tuesday",
-			spots: 5,
+	},
+	3: {
+		id: 3,
+		time: "2pm",
+	},
+	4: {
+		id: 4,
+		time: "3pm",
+		interview: {
+			student: "Archie Andrews",
+			interviewer: {
+				id: 4,
+				name: "Cohana Roy",
+				avatar: "https://i.imgur.com/FK8V841.jpg",
+			},
 		},
-		{
-			id: 3,
-			name: "Wednesday",
-			spots: 0,
-		},
-	];
+	},
+	5: {
+		id: 5,
+		time: "4pm",
+	},
+};
+
+export default function Application(props) {
+	const [day, setDay] = useState("Monday");
+
+	const appointmentDisplay = Object.values(appointments).map((appointment) => {
+		return (
+			<Appointment
+				key={appointment.id}
+				{...appointment}
+			/>
+		);
+	});
 
 	return (
 		<main className="layout">
@@ -36,20 +85,20 @@ export default function Application(props) {
 				<hr className="sidebar__separator sidebar--centered" />
 				<nav className="sidebar__menu">
 					<DayList
-                        days={days} 
-                        value={day} 
-                        onChange={setDay} 
+						days={days}
+						value={day}
+						onChange={setDay}
 					/>
 				</nav>
 				<img
 					className="sidebar__lhl sidebar--centered"
 					src="images/lhl.png"
 					alt="Lighthouse Labs"
-				/>{" "}
+				/>
 			</section>
-			<section className="schedule">
-				{/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-			</section>
+            <section className="schedule">
+                {appointmentDisplay}
+            </section>
 		</main>
 	);
 }
