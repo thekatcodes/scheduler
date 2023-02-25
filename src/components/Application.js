2194d65b89c0344f9c66bd79908d0b67b34e5836
Katie Liu <katieliu59@gmail.com>
2023-02-25 17:19:12 -0500

    Remove unused code

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,45 +7,6 @@ import Appointment from "./Appointment";
 import { getAppointmentsForDay } from "helpers/selectors";
 import "components/Application.scss";
 
-// const appointments = {
-// 	1: {
-// 		id: 1,
-// 		time: "12pm",
-// 	},
-// 	2: {
-// 		id: 2,
-// 		time: "1pm",
-// 		interview: {
-// 			student: "Lydia Miller-Jones",
-// 			interviewer: {
-// 				id: 3,
-// 				name: "Sylvia Palmer",
-// 				avatar: "https://i.imgur.com/LpaY82x.png",
-// 			},
-// 		},
-// 	},
-// 	3: {
-// 		id: 3,
-// 		time: "2pm",
-// 	},
-// 	4: {
-// 		id: 4,
-// 		time: "3pm",
-// 		interview: {
-// 			student: "Archie Andrews",
-// 			interviewer: {
-// 				id: 4,
-// 				name: "Cohana Roy",
-// 				avatar: "https://i.imgur.com/FK8V841.jpg",
-// 			},
-// 		},
-// 	},
-// 	5: {
-// 		id: 5,
-// 		time: "4pm",
-// 	},
-// };
-
 export default function Application(props) {
 	const [state, setState] = useState({
 		day: "Monday",
@@ -55,7 +16,6 @@ export default function Application(props) {
 	});
 
 	const setDay = (day) => setState({ ...state, day });
-	const setDays = (days) => setState((prev) => ({ ...prev, days }));
 
 	useEffect(() => {
 		Promise.all([
