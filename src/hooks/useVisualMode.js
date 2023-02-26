a4e7c96d2af6a13cfe478cb76f14c47e56ae1779
Katie Liu <katieliu59@gmail.com>
2023-02-26 03:00:45 -0500

    Create custom Hook useVisualMode that manages the visual mode of components

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+	const [mode, setMode] = useState(initial);
+	const [history, setHistory] = useState([initial]);
+
+	const transition = function (newMode, replace = false) {
+        setMode(newMode);
+        
+		setHistory((prev) => { 
+            if (replace) {
+                //replaces history
+                return [...prev.slice(0,-1), newMode]
+            } else {
+                //adds to history
+                return [...prev, newMode]
+            }
+		});
+    };
+    
+    const back = function() {
+        //goes back in history
+		if (history.length > 1) {
+			const prevHistory = history.slice(0, -1);
+			setHistory(prevHistory);
+			setMode(prevHistory[prevHistory.length - 1]);
+		}
+	};
+	return { mode, transition, back };
+}
