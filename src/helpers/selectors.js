22074460cf4cbdd1a5307684569f9c552e65a138
Katie Liu <katieliu59@gmail.com>
2023-02-26 14:45:43 -0500

    Create Interviewers Selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -18,15 +18,32 @@ export const getAppointmentsForDay = (state, day) => {
 export const getInterview = (state, interview) => {
 	// console.log({...interview}); /* -> log { student: 'Archie Cohen', interviewer: 2 } */
 
-    
 	if (!interview) {
-        return null;
-    } else {
-        const interviewer = state.interviewers[interview.interviewer];
-        return {
-            ...interview,
-            interviewer: { ...interviewer }
-        }
-    }
-    
+		return null;
+	} else {
+		const interviewer = state.interviewers[interview.interviewer];
+		return {
+			...interview,
+			interviewer: { ...interviewer },
+		};
+	}
+};
+
+export const getInterviewersForDay = (state, day) => {
+	//... returns an array of appointments for that day
+	const selectDay = state.days.find((days) => days.name === day);
+
+	if (!selectDay) {
+		return [];
+	}
+
+	const interviewersAppointments = selectDay.interviewers;
+	// console.log(interviewersAppointments);
+	// console.log(state);
+	const selectInterviewers = interviewersAppointments.map(
+		(interviewer) => state.interviewers[interviewer]
+	);
+
+	// console.log(selectInterviewers)
+	return selectInterviewers;
 };
