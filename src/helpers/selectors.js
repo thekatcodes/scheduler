a9633fc2789c34538ee385f2cf102b4ea0e313a9
Katie Liu <katieliu59@gmail.com>
2023-02-25 23:58:53 -0500

    Create getInterview function to return null if no interview is booked or return an object with the interviewer data

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,16 +1,32 @@
-export function getAppointmentsForDay(state, day) {
+export const getAppointmentsForDay = (state, day) => {
 	//... returns an array of appointments for that day
-    const selectDay = state.days.find((days) => days.name === day);
-    
-    if (!selectDay) {
-        return [];
-    }
-    
-    const dayAppointments = selectDay.appointments;
+	const selectDay = state.days.find((days) => days.name === day);
+
+	if (!selectDay) {
+		return [];
+	}
+
+	const dayAppointments = selectDay.appointments;
 
 	const selectAppointment = dayAppointments.map(
 		(appointment) => state.appointments[appointment]
-    );
-    
+	);
+
 	return selectAppointment;
-}
+};
+
+export const getInterview = (state, interview) => {
+	// console.log({...interview}); /* -> log { student: 'Archie Cohen', interviewer: 2 } */
+
+    
+	if (!interview) {
+        return null;
+    } else {
+        const interviewer = state.interviewers[interview.interviewer];
+        return {
+            ...interview,
+            interviewer: { ...interviewer }
+        }
+    }
+    
+};
