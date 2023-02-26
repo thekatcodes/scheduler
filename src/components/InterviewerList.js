5c4c87925caac2094d184b2201b4e257454d8e71
Katie Liu <katieliu59@gmail.com>
2023-02-26 14:09:14 -0500

    Convert interviewers object into array of object

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,18 +1,19 @@
 import React from "react";
-import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem";
+import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
- 
-	let interviewers = props.interviewers;
-	const interviewerDisplay = interviewers.map((interviewer) => {
+    const { interviewers, value, onChange } = props;
+    console.log(interviewers)
+    const ObjtoArray = (obj) => Object.assign([], Object.values(obj));
+	const interviewerDisplay = ObjtoArray(interviewers).map((interviewer) => {
         return (
             <InterviewerListItem
                 key={interviewer.id}
                 name={interviewer.name}
                 avatar={interviewer.avatar}
-                selected={interviewer.id === props.value}	
-                setInterviewer={() => props.onChange(interviewer.id)}
+                selected={interviewer.id === value}	
+                setInterviewer={() => onChange(interviewer.id)}
             />
 			
 		);
