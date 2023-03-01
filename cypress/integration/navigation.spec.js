5b4af0fee533621dc89abd0fb3cf067f00a09db1
Katie Liu <katieliu59@gmail.com>
2023-03-01 03:11:32 -0500

    Add test to find the Tuesday list item

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,11 @@
 describe("Navigation", () => {
-    it("should visit root", () => {
-      cy.visit("/");
-    });
-  });
+	it("should visit root", () => {
+		cy.visit("/");
+	});
+
+	it("should navigate to Tuesday", () => {
+        cy.contains("li", "Tuesday")
+        .click()
+        .should("have.css", "background-color", "rgb(242, 242, 242)");
+	});
+});
